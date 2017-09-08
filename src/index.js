ac425a9959039e024dd897df0a2209e3f5aa66ba
Nic Tamura <nictamura@gmail.com>
2017-09-07 23:27:30 -0700

    heroku path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import '%PUBLIC_URL%/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
